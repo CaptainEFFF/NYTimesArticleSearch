854fb3646b4555141a89cb9575ef193f4bd1b4ed
KDW <kyle.whittemore@gmail.com>
2019-04-27 13:31:42 -0400

    add html template string

--- javascript/app.js
+++ javascript/app.js
@@ -7,14 +7,26 @@ var endyear = 20170101
 // var year = $("#startYear")
 var apiKey = "Nv8KKzHV8oe7X8z5PYJH13PI2BFaMdiG"
 // var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search +  "&fq=pub_year:(" + year +")&api-key=" + apiKey
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search +  "&facet=true&begin_date=" + beginyear + "&end_date=" + endyear + "&api-key=" + apiKey
-
-
-
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&facet=true&begin_date=" + beginyear + "&end_date=" + endyear + "&facet=true&sort=relevance&api-key=" + apiKey;
 
 $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response)
+}).then(function (response) {
+    var responseData = response.response;
+    console.table(responseData.docs);
+    var headline = responseData.docs[0].headline.main;
+    var lead = responseData.docs[0].lead_paragraph;
+    var date = responseData.docs[0].pub_date;
+
+    returnDiv = `
+        <div>
+            <div id="headline">` + headline + `</div>
+            <div id="lead">` + lead + `</div>
+            <div id="date">`+ date +`</div>
+        </div>`
+
+    $('#return-div').html(returnDiv);
   });
+
+
