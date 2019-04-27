46915c799261a7b704a8c5e4644c4fc8a52f2e3d
CaptainEFFF <davidbanville4@gmail.com>
2019-04-27 13:42:43 -0400

    ajax writes to document

--- javascript/app.js
+++ javascript/app.js
@@ -16,35 +16,31 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + s
 $.ajax({
     url: queryURL,
     method: "GET"
-<<<<<<< HEAD
   }).then(function(response) {
       console.log(response);
-
-    var payload = response.response.docs;
-    for(var i = 0; i < payload.length; i ++){
-    console.log(payload[i].headline.main);
+      
+      var payload = response.response.docs;
+     
+      for(var i = 0; i < payload.length; i ++){
+          var headline = payload[i].headline.main;
+          var lead = payload[i].lead_paragraph;
+          var date = payload[i].pub_date;
+          console.log(payload[i].headline.main);
     console.log(payload[i].lead_paragraph);
     console.log(payload[i].web_url);
     console.log(" ");
+    returnDiv = `
+    <div>
+    <div id="headline">` + headline + `</div>
+    <div id="lead">` + lead + `</div>
+    <div id="date">`+ date +`</div>
+    </div></br>`
+    $('#return-div').append(returnDiv);
     
     }
-=======
-}).then(function (response) {
-    var responseData = response.response;
-    console.table(responseData.docs);
-    var headline = responseData.docs[0].headline.main;
-    var lead = responseData.docs[0].lead_paragraph;
-    var date = responseData.docs[0].pub_date;
+    // console.table(responseData.docs);
 
-    returnDiv = `
-        <div>
-            <div id="headline">` + headline + `</div>
-            <div id="lead">` + lead + `</div>
-            <div id="date">`+ date +`</div>
-        </div>`
 
-    $('#return-div').html(returnDiv);
->>>>>>> ce0eccea234ca2ca5f978f33bab0c64416cc00f6
   });
 
 
